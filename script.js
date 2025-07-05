6ad3253bc8be0648a024a1e7ae53023de74a40a4
spnashish <ashishsharma.it.in@gmail.com>
2025-07-05 10:17:59 +0530

    Function Completed

--- script.js
+++ script.js
@@ -13,64 +13,70 @@ let b=9;
 //1. Primitive 2.  Non-Primitive
 //number String Undefined Null Boolean BigInt Symbol
 //Object
-const Student = {
-    name: "Ashish",
-    age: 23,
-    cgpa: 7.3,
-    isPass: true
-};
+// const Student = {
+//     name: "Ashish",
+//     age: 23,
+//     cgpa: 7.3,
+//     isPass: true
+// };
 //Type Conversion
-let res=3;
-console.log(res);
-console.log(typeof res);
-//function
-function fun()
-{
-    console.log("This is the Function Named Fun()");
-    fun1(a,b)
-}
-function fun1(a,b)
-{
-    console.log("This is the Function Named Fun1()");
-    console.log(a+b);
-    fun2(a,b)
-}
-const fun2 = (a,b)=>{
-    console.log("This is the Function Named Fun2() with Arrow ");
-    console.log(a+b);
-    fun3(a,b)
-}
-const fun3 = (a,b)=>{
-    console.log("This is the Function Named Fun3()");
-    console.log(a+b);
+// let res=3;
+// console.log(res);
+// console.log(typeof res);
+// //function
+// function fun()
+// {
+//     console.log("This is the Function Named Fun()");
+//     fun1(a,b)
+// }
+// function fun1(a,b)
+// {
+//     console.log("This is the Function Named Fun1()");
+//     console.log(a+b);
+//     fun2(a,b)
+// }
+// const fun2 = (a,b)=>{
+//     console.log("This is the Function Named Fun2() with Arrow ");
+//     console.log(a+b);
+//     fun3(a,b)
+// }
+// const fun3 = (a,b)=>{
+//     console.log("This is the Function Named Fun3()");
+//     console.log(a+b);
     
-}
+// }
 
-function name(a)
-{
-    console.log("This is the function Named Name");
-    console.log("hello "+a);
-}
+// function name(a)
+// {
+//     console.log("This is the function Named Name");
+//     console.log("hello "+a);
+// }
 
-fun();
-name("Ashish")
+// // fun();
+// // name("Ashish")
 
-const add=(num1,num2)=>{
-    return num1+num2;
-}
-console.log(add(4,5));
+// const add=(num1,num2)=>{
+//     return num1+num2;
+// }
+// console.log(add(4,5));
 
-//Addtion In Function 
+// //Addtion In Function 
 
-const sun = function(temp,temp1){
-    console.log(temp*temp1);
-    console.log("Advanced Function");
-}
-sun(3,6);
+// const sun = function(temp,temp1){
+//     console.log(temp*temp1);
+//     console.log("Advanced Function");
+// }
+// sun(3,6);
 
-(function(){
-    console.log("Ashish Time Lagaa Aur bathe phir Deekh");
-})();
+// (function(){
+//     console.log("Ashish Time Lagaa Aur bathe phir Deekh");
+// })();
+
+
+
+
+
+
 
 
 
