e4da9f01c2c050f0eaac048cac768b84b29c6903
spnashish <ashishsharma.it.in@gmail.com>
2025-07-05 11:29:02 +0530

    Completed Array Data Structure

--- script.js
+++ script.js
@@ -72,18 +72,103 @@ let b=9;
 //     console.log("Ashish Time Lagaa Aur bathe phir Deekh");
 // })();
 
+// console.log("Start Kar Rha hu Array Se ");
+// console.log("Ab Option Nhi hai Bhai Kro Ab Bahut amain katt li ab nbatho");
+
+// let name=["ashish ","Ayush" ,"Harshit"];
+// console.log(name);
+
+// name.push("Vishal");
+// console.log(name);
+
+// name.pop();
+// console.log(name);
+
+// let name2=["Sharma"];
+// name.concat(name2);
+
+// name.unshift("Hello");
+// console.log(name);
+
+// name.shift("Kaam KAro");
+// console.log(name);
+
+// name.slice(2,4);
+// console.log(name);
+
+// name.splice(0,2,"hh");
+// console.log(name);
 
 
+let num=[1,2,3,4,5,6,7,8];
+let num2=[2,3,2,3,23,2,1,2];
 
 
+for(let i=0;i<num.length;i++)
+{
+    console.log(num[i]);
+}
+
+//forEach loop
+
+num.forEach((val,ind,arr)=>{
+    console.log("Index "+ind+"   value"+val+"    Array"+arr);
+})
+
+//map Method
+
+let newarr=num.map((val)=>{
+    return val*4;
+})
+console.log(newarr);
 
 
+console.log(num.concat(num2));
+//console.log(num2.sort());
+console.log(num2.reverse());
 
+console.log("For of Loop");
+for(let arr of num2)
+{
+    console.log(arr);
+}
+console.log("For in Loop");
 
+for(let arr in num2)
+{
+    console.log(arr);
+}
 
+//Multidimensional Array
 
+let mat=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
 
+for(let i=0;i<mat.length;i++)
+{
+    for(let j=0;j<mat[i].length;j++)
+    {
+        console.log(mat[i][j]);
+    }
+}
 
+for(let ar of mat)
+{
+    for(let arr of ar)
+    {
+        console.log(arr);
+
+    }
+}
+
+mat.forEach((val,ind,mat)=>{
+    val.forEach(data=>{
+        console.log(data);
+    })
+})
 
 
 
