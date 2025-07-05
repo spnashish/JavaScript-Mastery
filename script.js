23f6670779db2aacb5262b4a8196b1ff6566d71d
spnashish <ashishsharma.it.in@gmail.com>
2025-07-05 09:52:19 +0530

    Learned Till Function

---
+++ script.js
@@ -0,0 +1,98 @@
+console.log("JavaScript Mastery");
+console.log("Work Hard Till YOu Not Succeed");
+//alert("Work Harder Ashish");
+//Addtion of Two Number
+// let a=prompt("Enter The NUmber");
+// let b=prompt("Enter the Second Number");
+//console.log(a+b);
+//Variable
+let a=0;
+let b=9;
+//console.log(a+b);
+//dataType
+//1. Primitive 2.  Non-Primitive
+//number String Undefined Null Boolean BigInt Symbol
+//Object
+const Student = {
+    name: "Ashish",
+    age: 23,
+    cgpa: 7.3,
+    isPass: true
+};
+//Type Conversion
+let res=3;
+console.log(res);
+console.log(typeof res);
+//function
+function fun()
+{
+    console.log("This is the Function Named Fun()");
+    fun1(a,b)
+}
+function fun1(a,b)
+{
+    console.log("This is the Function Named Fun1()");
+    console.log(a+b);
+    fun2(a,b)
+}
+const fun2 = (a,b)=>{
+    console.log("This is the Function Named Fun2() with Arrow ");
+    console.log(a+b);
+    fun3(a,b)
+}
+const fun3 = (a,b)=>{
+    console.log("This is the Function Named Fun3()");
+    console.log(a+b);
+    
+}
+
+function name(a)
+{
+    console.log("This is the function Named Name");
+    console.log("hello "+a);
+}
+
+fun();
+name("Ashish")
+
+const add=(num1,num2)=>{
+    return num1+num2;
+}
+console.log(add(4,5));
+
+//Addtion In Function 
+
+const sun = function(temp,temp1){
+    console.log(temp*temp1);
+    console.log("Advanced Function");
+}
+sun(3,6);
+
+(function(){
+    console.log("Ashish Time Lagaa Aur bathe phir Deekh");
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
